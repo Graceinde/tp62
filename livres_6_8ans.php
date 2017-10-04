c220893f4235d0a2f0122666845e3004fdb1506d
Graceinde <gracesoundron548@gmail.com>
2017-10-04 16:49:37 -0400

    Commit initial

---
+++ livres_6_8ans.php
@@ -0,0 +1,17 @@
+
+<?php
+require_once ('category.php');
+require_once ('views/header.php');
+?>
+
+
+<?php
+foreach ($livres as $liv => $livre) ?>
+<div id="livres-de6-8ans">
+    <img src="<?=$livres[BOOK_IMG]?>">
+    <p><?=BOOK_DESC?></p>
+
+    <div>
+?>
+<?php
+require_once 'views/footer.php';
